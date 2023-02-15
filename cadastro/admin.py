7bd56849170d3ac5ff6f110e506b516523fb2cf1
bWX1172881 <brdc.cxiii1@h-partners.com>
2023-02-15 13:56:58 -0300

    Update Project

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 
-from cadastro.models import Cidade
+from cadastro.models import Cidade, Estado, Pais
 
 # Register your models here.
 
+
+class EstadoInline(admin.TabularInline):
+
+    model = Estado
+
+
+class PaisAdmin(admin.ModelAdmin):
+
+    fields = ('nome',)
+    inlines = [
+        EstadoInline
+    ]
+
+
+admin.site.register(Pais, PaisAdmin)
+admin.site.register(Estado)
 admin.site.register(Cidade)
